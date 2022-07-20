f2868e8fdca68668680dc57248f7273cf6b0450d
Rafal <rafalkubala97@gmail.com>
2022-07-20 20:07:07 +0200

    Added routes and basic views.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
+import { Header } from './components/layout/Header';
+import { Auth } from './views/Auth/Auth';
+import { Hr } from './views/Hr/Hr';
+import { Student } from './views/Student/Student';
+import { Admin } from './views/Admin/Admin';
 import './App.css';
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header/>
+      <Routes>
+        <Route path="/auth" element={<Auth/>}></Route>
+        <Route path="/hr" element={<Hr/>}></Route>
+        <Route path="/student" element={<Student/>}></Route>
+        <Route path="/admin" element={<Admin/>}></Route>
+      </Routes>
+    </>
   );
 }
-
-export default App;
