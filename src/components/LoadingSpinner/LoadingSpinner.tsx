67b082c7b2531a4b4392aa6ec3c0e35f91cef901
Kagr7 <kamgre7@gmail.com>
2022-07-20 22:23:44 +0200

    Added chakra ui with loading spinner

---
+++ src/components/LoadingSpinner/LoadingSpinner.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Center, Spinner} from "@chakra-ui/react";
+
+export const LoadingSpinner = () => (
+    <Center>
+        <Spinner
+            mt="20px"
+            thickness="4px"
+            speed="0.65s"
+            emptyColor="gray.200"
+            color="red.500"
+            size="xl"
+        />
+    </Center>)
