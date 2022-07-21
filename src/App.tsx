241beea60bb2c70edd0e96241a0f21695ba66dc5
Kagr7 <kamgre7@gmail.com>
2022-07-21 23:06:33 +0200

    Added view with styles of login panel

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {ChakraProvider} from '@chakra-ui/react';
 import {LoadingSpinner} from "./components/LoadingSpinner/LoadingSpinner";
+import {LoginForm} from "./components/Forms/LoginForm";
 
 function App() {
     return (
         <ChakraProvider>
+            <LoginForm/>
             <LoadingSpinner/>
         </ChakraProvider>
     );
--- src/components/Forms/LoginForm.tsx
+++ src/components/Forms/LoginForm.tsx
@@ -1,6 +1,81 @@
 import React from 'react';
+import {useFormik} from "formik";
+import {
+    Box,
+    Button,
+    Flex,
+    FormControl,
+    Input, Link, Stack, Text,
+    VStack
+} from "@chakra-ui/react";
 
-export const LoginForm = ()=>{
-    console.log('init commit')
-    return (<h1>Works</h1>)
+export const LoginForm = () => {
+    const formik = useFormik({
+        initialValues: {
+            email: "",
+            password: "",
+        },
+        onSubmit: async(values) => {
+
+          await fetch(`http://localhost:3001/auth`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(values),
+            });
+        }
+    });
+
+    return (
+        <Flex align="center" justify="center" bg="#222224" height="100vh">
+            <Box bg="#222224" p={6} rounded="md" border="1px" mt="20px" mb="20px">
+                <form onSubmit={formik.handleSubmit}>
+                    <VStack spacing={4} align="flex-start">
+                        <FormControl>
+                            <Input
+                                id="email"
+                                name="email"
+                                type="email"
+                                variant="filled"
+                                placeholder="email"
+                                onChange={formik.handleChange}
+                                value={formik.values.email}
+                                bgColor="#292A2B"
+                                color="#DADADA"
+                            />
+                        </FormControl>
+                        <FormControl>
+                            <Input
+                                id="password"
+                                name="password"
+                                type="password"
+                                placeholder="password"
+                                variant="filled"
+                                bgColor="#292A2B"
+                                color="#DADADA"
+                                onChange={formik.handleChange}
+                                value={formik.values.password}
+                            />
+                        </FormControl>
+                        <Stack spacing={10}>
+                            <Stack
+                                direction={{base: 'column', sm: 'row'}}
+                                align={'start'}
+                                justify={'flex-end'}>
+                                <Link color="#DADADA">Zapomniałeś hasła?</Link>
+                            </Stack>
+                            <Stack direction={{base: 'row', sm: 'row'}}>
+                                <Text color="#DADADA">Nie masz konta?</Text>
+                                <Text as='u'> <Link color="#DADADA"> Zarejestruj sie</Link></Text>
+                                <Button type="submit" colorScheme="red">
+                                    Zaloguj się
+                                </Button>
+                            </Stack>
+                        </Stack>
+                    </VStack>
+                </form>
+            </Box>
+        </Flex>
+    );
 }
