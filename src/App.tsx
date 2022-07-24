6eade964943c726555d0f6982bd200c0574a8157
Kagr7 <kamgre7@gmail.com>
2022-07-24 19:50:39 +0200

    Removed spinner view of app.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import {ChakraProvider} from '@chakra-ui/react';
-import {LoadingSpinner} from "./components/LoadingSpinner/LoadingSpinner";
 import {LoginForm} from "./components/Forms/LoginForm";
 
 function App() {
     return (
         <ChakraProvider>
             <LoginForm/>
-            <LoadingSpinner/>
         </ChakraProvider>
     );
 }
