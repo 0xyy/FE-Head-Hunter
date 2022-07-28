9a2d6033bea8312e7f7dc750864238cd4eccddfb
kamil <you@example.com>
2022-07-28 18:14:16 +0200

    slice example

--- src/redux/reducers/authReducer.ts
+++ src/redux/reducers/authReducer.ts
@@ -1,4 +1,18 @@
-import { createReducer } from "@reduxjs/toolkit";
+import {createReducer, createSlice} from "@reduxjs/toolkit";
 
-const authReducer = createReducer([], builder => {})
-export default authReducer
+
+const authSlice = createSlice({
+    name: "auth",
+    initialState: {
+        isLoggedIn: false,
+        userType: null
+    },
+    reducers: {
+        logIn: (state, action) => {},
+        logOut: (state, action) => {},
+        getToken: (state, action) => {},
+    }
+})
+
+export const {logIn, logOut, getToken} = authSlice.actions
+export default authSlice.reducer
