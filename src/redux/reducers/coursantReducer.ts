7ba6a41e58486c491911594154ef69680e48fa3e
kamil <you@example.com>
2022-07-28 17:18:35 +0200

    created basic structure for reducers

---
+++ src/redux/reducers/coursantReducer.ts
@@ -0,0 +1,4 @@
+import { createReducer } from "@reduxjs/toolkit";
+
+const coursantReducer = createReducer([], builder => {})
+export default coursantReducer
---
+++ src/redux/reducers/hrReducer.ts
@@ -0,0 +1,4 @@
+import { createReducer } from "@reduxjs/toolkit";
+
+const hrReducer = createReducer([], builder => {})
+export default hrReducer
