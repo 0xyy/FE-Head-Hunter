3e496a043d192cfb018fdefad53685c84b30eb1b
Marcin Biłka <98228475+BilkaDev@users.noreply.github.com>
2022-07-28 19:03:35 +0200

    Add http-hook.

---
+++ src/common/hooks/http-hook.ts
@@ -0,0 +1,50 @@
+import {useCallback, useState} from "react";
+import {RecoverPasswordRequest,EditPasswordRequest, ActivateUserRequest} from 'types';
+
+export type ReqBody = (
+    | EditPasswordRequest
+    | RecoverPasswordRequest
+    | ActivateUserRequest
+    | FormData
+    | null
+    )
+
+export const useHttpClient = () => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState<null | string>();
+
+
+    let statusError = 500;
+    const sendRequest = useCallback(
+        async (
+            url: string,
+            method = 'GET',
+            body: ReqBody = null,
+            headers = {}
+        ) => {
+            try {
+                setIsLoading(true);
+                const response = await fetch(`http://localhost:3001${url}`, {
+                    method,
+                    headers,
+                    body: body && {body: body instanceof FormData ? body : JSON.stringify(body)}.body,
+                });
+
+                const responseData = await response.json();
+                setIsLoading(false);
+                if (!responseData.isSuccess) {
+                    setError(responseData.message);
+                    return responseData;
+                }
+                return responseData;
+            } catch (e: any) {
+                setError(statusError === 500 ? 'Ops... Proszę spróbować za kilka minut.' : e.message);
+                setIsLoading(false);
+                throw e;
+            }
+        }, []);
+    const clearError = () => {
+        setError(null);
+    };
+    return {isLoading, error, sendRequest, setError, clearError};
+};
