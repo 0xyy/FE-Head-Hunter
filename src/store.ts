b142996b4b5eef393f9ebaed0e3999b96eda80ec
kamil <you@example.com>
2022-07-28 17:46:18 +0200

    store init

--- src/store.ts
+++ src/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit'
 import hrReducer from "./redux/reducers/hrReducer";
 import userReducer from "./redux/reducers/userReducer";
 import coursantReducer from "./redux/reducers/coursantReducer";
+import authReducer from "./redux/reducers/authReducer";
 
 const store = configureStore({
     reducer: {
         user: userReducer,
         hr: hrReducer,
         coursant: coursantReducer,
+        auth: authReducer
     },
     // middleware: (getDefaultMiddleware) => {},
 })
