ad20d7b3bd7a6825884180569170a8fc060049c6
Tommy Phan <tommyphan2004.tp@gmail.com>
2022-07-29 15:23:06 +0200

    Fixed styling and seperated forms into individual components.

--- src/views/Admin/Admin.tsx
+++ src/views/Admin/Admin.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { Box, Button, Center, Flex, FormControl, Heading, HStack, Input, Link, Stack, Text } from '@chakra-ui/react';
+import { Box, Button, Center, Flex, FormControl, Heading, HStack, Input, Stack } from '@chakra-ui/react';
+import { AddHrForm } from '../../components/Forms/AddHrForm';
 
 import './Admin.css';
+import { AddStudentsForm } from '../../components/Forms/AddStudentsForm';
 
 export const Admin = () => {
     return <>
@@ -12,73 +14,12 @@ export const Admin = () => {
                 </Center>
                 <HStack align="flex-start" justify="space-between">
                     <Box w="50%">
-                        <Heading color="white" as="h3" size="lg" mb="20px">Dodaj kursanta</Heading>
-                        <form>
-                            <FormControl>
-                                <Input
-                                    id="file"
-                                    name="file"
-                                    type="file"
-                                    bgColor="#292A2B"
-                                    color="#DADADA"
-                                />
-                            </FormControl>
-                            <Stack width="100%" pt="10px">
-                                <Button type="submit" colorScheme="red">Dodaj</Button>
-                            </Stack>
-                        </form>
+                        <Heading color="white" as="h3" size="lg" mb="20px">Dodaj kursantów (plik .json)</Heading>
+                        <AddStudentsForm/>
                     </Box>
                     <Box w="50%">
                         <Heading color="white" as="h3" size="lg" mb="20px">Dodaj HR-a</Heading>
-                        <form>
-                            <FormControl mb="10px">
-                                <Input
-                                    id="email"
-                                    name="email"
-                                    type="email"
-                                    placeholder="E-mail"
-                                    variant="filled"
-                                    bgColor="#292A2B"
-                                    color="#DADADA"
-                                />
-                            </FormControl>
-                            <FormControl mb="10px">
-                                <Input
-                                    id="fullName"
-                                    name="fullName"
-                                    type="text"
-                                    placeholder="Imię i nazwisko"
-                                    variant="filled"
-                                    bgColor="#292A2B"
-                                    color="#DADADA"
-                                />
-                            </FormControl>
-                            <FormControl mb="10px">
-                                <Input
-                                    id="company"
-                                    name="company"
-                                    type="text"
-                                    placeholder="Nazwa firmy"
-                                    variant="filled"
-                                    bgColor="#292A2B"
-                                    color="#DADADA"
-                                />
-                            </FormControl>
-                            <FormControl>
-                                <Input
-                                    id="maxReservedStudents"
-                                    name="maxReservedStudents"
-                                    type="number"
-                                    placeholder="Maksymalna liczba osób, jakie może dodać do listy 'Do rozmowy'."
-                                    variant="filled"
-                                    bgColor="#292A2B"
-                                    color="#DADADA"
-                                />
-                            </FormControl>
-                            <Stack width="100%" pt="10px">
-                                <Button type="submit" colorScheme="red">Dodaj</Button>
-                            </Stack>
-                        </form>
+                        <AddHrForm/>
                     </Box>
                 </HStack>
             </Box>
