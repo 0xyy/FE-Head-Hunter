31009f3b9a8e035d99f0988e3fa72afd4abaa753
Marcin Biłka <98228475+BilkaDev@users.noreply.github.com>
2022-07-31 17:13:29 +0200

    Added back button in revcover-password.

--- src/components/Forms/RecoverPasswordForm.tsx
+++ src/components/Forms/RecoverPasswordForm.tsx
@@ -1,53 +1,54 @@
-import React, {useEffect, useState} from 'react';
-import {useFormik} from 'formik';
+import React, {useEffect, useState} from "react";
+import {useFormik} from "formik";
 import {
     Button,
     FormControl, FormErrorMessage,
     Input, Stack, useToast,
     VStack,
-} from '@chakra-ui/react';
+} from "@chakra-ui/react";
 import {useHttpClient} from "../../common/hooks/http-hook";
 import {InfoModal} from "../../common/components/InfoModal/InfoModal";
-import * as Yup from 'yup';
+import * as Yup from "yup";
+import {Link} from "react-router-dom";
 
 const LoginSchema = Yup.object().shape({
-    email: Yup.string().email('Podaj poprawny adres e-mail!').required('Wymagane!'),
+    email: Yup.string().email("Podaj poprawny adres e-mail!").required("Wymagane!"),
 });
 
 export const RecoverPasswordForm = () => {
         const [toastMessage, setToastMessage] = useState({
-            title: '',
-            body: '',
+            title: "",
+            body: "",
         });
         const {sendRequest, error, clearError} = useHttpClient();
         const toast = useToast();
         const formik = useFormik({
             initialValues: {
-                email: '',
-                password: '',
+                email: "",
+                password: "",
             },
             validationSchema: LoginSchema,
             onSubmit: async (values) => {
-                await sendRequest('/user/recover', 'POST', {
+                await sendRequest("/user/recover", "POST", {
                     email: values.email,
                 }, {
-                    'Content-Type': 'application/json',
+                    "Content-Type": "application/json",
                 });
                 setToastMessage({
-                    title: 'Sukces!',
-                    body: 'Jeśli e-mail jest prawidłowy. Nowe hasło zostanie wysłane.',
+                    title: "Sukces!",
+                    body: "Jeśli e-mail jest prawidłowy. Nowe hasło zostanie wysłane.",
                 });
             }
         });
 
         useEffect(() => {
-            if (toastMessage.title !== '' && toastMessage.body !== '') {
+            if (toastMessage.title !== "" && toastMessage.body !== "") {
                 const {title, body} = toastMessage;
 
                 toast({
                     title,
                     description: body,
-                    status: 'success',
+                    status: "success",
                     duration: 5000,
                     isClosable: true
                 });
@@ -56,7 +57,7 @@ export const RecoverPasswordForm = () => {
 
         return (
             <>
-                {error && <InfoModal isError message={error} onClose={clearError} title={'Nieudana próba!'}/>}
+                {error && <InfoModal isError message={error} onClose={clearError} title={"Nieudana próba!"}/>}
                 <form onSubmit={formik.handleSubmit}>
                     <VStack spacing={4} align="flex-start">
                         <FormControl isInvalid={!!formik.errors.email}>
@@ -77,9 +78,10 @@ export const RecoverPasswordForm = () => {
                         </FormControl>
                         <Stack spacing={10} width="100%" pt="10px">
                             <Stack
-                                direction={{base: 'column', sm: 'row'}}
-                                align={'center'}
-                                justify={'center'}>
+                                direction={{base: "column", sm: "row"}}
+                                align={"center"}
+                                justify={"center"}>
+                                <Link to="/"><Button>Powrót</Button></Link>
                                 <Button type="submit" colorScheme="red">
                                     Odzyskaj hasło
                                 </Button>
